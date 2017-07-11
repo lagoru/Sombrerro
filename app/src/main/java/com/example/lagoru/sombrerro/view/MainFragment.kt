f69fa211ac1a76a203d38fb15fb413efdce61c85
Adrian Ziemek <adrian.ziemek@smagabakery.com>
2017-07-11 21:59:27 +0200

    some work on render logic

--- app/src/main/java/com/example/lagoru/sombrerro/view/MainFragment.kt
+++ app/src/main/java/com/example/lagoru/sombrerro/view/MainFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.data.model.Topic
 import com.example.lagoru.sombrerro.R
 import com.example.lagoru.sombrerro.mvi.MviFragment
 import com.example.lagoru.sombrerro.mvi.ViewState
+import kotlinx.android.synthetic.main.fragment_main.*
 import javax.inject.Inject
 
 /**
@@ -29,5 +32,15 @@ class MainFragment : MviFragment<MainFragment, MainFragmentViewState, MainFragme
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    fun renderTopic(topics: List<Topic>) {
+        circleLayout.removeAllViews()
+        for (topic in topics) {
+            val textView = TextView(context)
+            textView.text = topic.title
+            textView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            circleLayout.addView(textView)
+        }
     }
 }
--- app/src/main/java/com/example/lagoru/sombrerro/view/MainFragmentPresenter.kt
+++ app/src/main/java/com/example/lagoru/sombrerro/view/MainFragmentPresenter.kt
@@ -6,7 +6,7 @@ import com.example.lagoru.sombrerro.mvi.MviPresenter
  * Created by lagoru on 21.06.17.
  */
 
-class MainFragmentPresenter : MviPresenter<MainFragment,MainFragmentViewState>{
+class MainFragmentPresenter : MviPresenter<MainFragment, MainFragmentViewState> {
 
     override var viewState: MainFragmentViewState
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
@@ -20,5 +20,4 @@ class MainFragmentPresenter : MviPresenter<MainFragment,MainFragmentViewState>{
     override fun detachView(retainInstance: Boolean) {
 
     }
-
 }
